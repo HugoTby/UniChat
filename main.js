a5b9c8666d64ba9cfb967c21cb5fab8bc23c3613
Hugo Tabary <97984737+HugoTby@users.noreply.github.com>
2023-05-10 22:45:46 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,8 +2,45 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 const ipc = ipcMain
 
-function createWindow() {
-    const win = new BrowserWindow({
+///// PHP SERVER CREATION /////
+const PHPServer = require('php-server-manager');
+
+let server
+  if (process.platform === 'win32') {
+
+server = new PHPServer({
+    php: `${__dirname}/php/php.exe`,
+    port: 5555,
+    directory: __dirname,
+    directives: {
+        display_errors: 1,
+        expose_php: 1
+    }
+    });
+  } else {
+
+server = new PHPServer({
+  
+    port: 5555,
+    directory: __dirname,
+    directives: {
+        display_errors: 1,
+        expose_php: 1
+    }
+});
+};
+
+//////////////////////////
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+
+  server.run();
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
         width: 1200,
         height: 680,
         minWidth: 940,
@@ -17,41 +54,68 @@ function createWindow() {
         }
     })
 
-    win.loadFile('src/index.html')
-    win.setBackgroundColor('#343B48')
+  // and load the index.html of the app.
+  mainWindow.loadURL('http://'+server.host+':'+server.port+'/src/login/connexion.html')
+  mainWindow.setBackgroundColor('#343B48')
 
-    //// MINIMIZE APP
+/*
+mainWindow.loadURL(url.format({
+  pathname: path.join(__dirname, 'index.php'),
+  protocol: 'file:',
+  slashes: true
+}))
+*/
+ const {shell} = require('electron')
+ shell.showItemInFolder('fullPath')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function () {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    // PHP SERVER QUIT
+    server.close();
+    mainWindow = null;
+  })
+
+  //// MINIMIZE APP
     ipc.on('minimizeApp', () =>{
         console.log('Clicked on Minimize Btn')
-        win.minimize()
+        mainWindow.minimize()
     })
 
     //// MAXIMISE RESTORE APP
     ipc.on('maximizeRestoreApp', () =>{
-        if (win.isMaximized()) {
+        if (mainWindow.isMaximized()) {
             console.log('Clicked on Restore')
-            win.restore()
+            mainWindow.restore()
         } else {
             console.log('Clicked on Maximize')
-            win.maximize()
+            mainWindow.maximize()
         }
     })
     // Check if is maximized
-    win.on('maximise', () =>{
-        win.webContents.send('isMaximized')
+    mainWindow.on('maximise', () =>{
+      mainWindow.webContents.send('isMaximized')
     })
     // Check if is restored
-    win.on('unmaximise', () =>{
-        win.webContents.send('isRestored')
+    mainWindow.on('unmaximise', () =>{
+      mainWindow.webContents.send('isRestored')
     })
 
     //// CLOSE APP
     ipc.on('closeApp', () =>{
         console.log('Clicked on Close Btn')
-        win.close()
+        mainWindow.close()
     })
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     createWindow()
 
@@ -67,3 +131,7 @@ app.on('window-all-closed', () => {
         app.quit()
     }
 })
+
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
